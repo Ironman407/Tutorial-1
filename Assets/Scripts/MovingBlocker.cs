30d45ba2fb1489fc4aab242210cdd4bb24f44950
Ironman407 <42649627+Ironman407@users.noreply.github.com>
2018-09-23 08:58:14 -0400

    Commit Changes For Challenge

---
+++ Assets/Scripts/MovingBlocker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBlocker : MonoBehaviour {
+
+    public float speed = 2.5f;
+   
+    
+	// Update is called once per frame
+	void Update ()
+    {
+
+        transform.position = new Vector3(Mathf.PingPong(Time.time * speed, 5), transform.position.y, transform.position.z);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,29 +8,45 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Text countText;
     public Text winText;
+    public Text pickUpText;
+    public GameObject Player;
 
     private Rigidbody rb;
-    private int count;
+    private int score, pickUpCount;
+    private Renderer re;
 
-	void Start ()
+    void Start ()
     {
         rb = GetComponent<Rigidbody>();
-        count = 0;
-        SetCountText ();
+        score = 0;
+        pickUpCount = 0;
+        SetAllText ();
         winText.text = "";
 	}
-	
-	
-	void FixedUpdate ()
+
+    void Update()
     {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    void FixedUpdate ()
+    {
+
+        float colorR = Mathf.Abs(Player.transform.position.x / 10);
+        float colorG = Mathf.Abs(Player.transform.position.y / 10);
+        float colorB = Mathf.Abs(Player.transform.position.z / 10);
+        Color colornow = new Vector4(colorR, colorG, colorB, 0.0f);
+        Player.GetComponent<Renderer>().material.color = colornow;
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce (movement * speed);
-        if (Input.GetKey("Escape"))
-            Application.Quit();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -38,15 +54,42 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive (false);
-            count = count + 1;
-            SetCountText ();
+            score = score + 1;
+            SetAllText ();
+        }
+
+        if (other.gameObject.CompareTag("Bad Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            score = score - 1;
+            SetAllText();
+        }
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            pickUpCount = pickUpCount + 1;
+            SetAllText ();
+        }
+        if (pickUpCount == 12)
+        {
+            transform.position = new Vector3(0, -12, -1);
         }
     }
-
-    void SetCountText ()
+    private void OnCollisionEnter(Collision collision)
     {
-        countText.text = "Count: " + count.ToString ();
-        if (count >= 12)
-            winText.text = "You Win!";
+        if (collision.gameObject.tag == "Enemy Blocker")
+            score = score - 1;
+        SetAllText();
+    }
+
+    void SetAllText ()
+    {
+        pickUpText.text = "Pick Ups Collectd: " + pickUpCount.ToString();
+
+        countText.text = "Score: " + score.ToString ();
+        if (pickUpCount == 24)
+        {
+            winText.text = "You won with a score of: " + score.ToString();
+        }
     }
 }
